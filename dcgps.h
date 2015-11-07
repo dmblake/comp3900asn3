86a056c9e67d5cd030b9b33ba2b6ba69b9a89c9b
Dylan Blake <dm_blake@live.com>
2015-11-06 16:48:15 -0800

    Fixed fixsource_t definition

--- dcgps.h
+++ dcgps.h
@@ -1,6 +1,13 @@
 #ifndef DCGPS_H
 #define DCGPS_H
 
+typedef struct fixsource_t
+{
+    char *spec;
+    char *server;
+    char *port;
+    char *device;
+} fixsource_t;
 typedef struct gps_data_t gps_data_t;
 #include <gps.h>
 #include <stdio.h>
